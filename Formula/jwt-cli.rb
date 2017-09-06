6de9abf497ad9f703778bc19dba865beecf39f54
Mike Engel <mike@mike-engel.com>
2017-09-06 00:12:43 -0400

    jwt-cli 1.2.0

--- Formula/jwt-cli.rb
+++ Formula/jwt-cli.rb
@@ -2,7 +2,7 @@ class JwtCli < Formula
   desc "Super fast CLI tool to decode and encode JWTs built in Rust"
   homepage "https://github.com/mike-engel/jwt-cli"
   url "https://github.com/mike-engel/jwt-cli/releases/download/1.1.0/jwt-cli-1.1.0-macOS.tar.gz"
-  version "1.1.0"
+  version "1.2.0"
   sha256 "97e9e6d776021d07e0ef85dc3873d379aed527529b6e9588ae072c83eb52819f"
   head "https://github.com/mike-engel/jwt-cli.git"
 
